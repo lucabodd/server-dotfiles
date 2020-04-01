d4841c731ff07c5863ce92a10a77bd54d8d4632e
Luca Bodini <lucabodd@gmail.com>
2020-04-01 19:04:35 +0200

    setup script

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ elif [[ $os == 'Linux' ]]; then
     $pkg_cmd = "apt-get install"
 fi
 
-sudo $pkg_cmd zsh
-sudo $pkg_cmd git
-sudo $pkg_cmd tmux
+$(sudo $pkg_cmd zsh)
+$(sudo $pkg_cmd git)
+$(sudo $pkg_cmd tmux)
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 wget -O ~/.tmux.conf https://raw.githubusercontent.com/lucabodd/server-dotfiles/master/.tmux.conf
