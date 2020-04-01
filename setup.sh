4585639a703eba29beb0153f3233eb380101cc5f
Luca Bodini <lucabodd@gmail.com>
2020-04-01 18:49:34 +0200

    setup script

--- setup.sh
+++ setup.sh
@@ -11,3 +11,4 @@ sudo $pkg_cmd git
 sudo $pkg_cmd tmux
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/lucabodd/server-dotfiles.git ~/
