aa4969c1a88274c56b49e1b9e7c1153843a20dbb
Luca Bodini <lucabodd@gmail.com>
2020-04-01 18:46:46 +0200

    setup script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+os = $(uname)
+
+if [[ $os == 'OpenBSD' ]]; then
+    $pkg_cmd = "pkg_add"
+elif [[ $os == 'Linux' ]]; then
+    $pkg_cmd = "apt-get install"
+fi
+
+sudo $pkg_cmd zsh
+sudo $pkg_cmd git
+sudo $pkg_cmd tmux
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
